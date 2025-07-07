999befd19b6cc4241b3b3d5173838e3d6669e588
mitsubhatt <mitscode1204@gmail.com>
2025-07-07 16:23:46 +0530

    Day 3 task added

---
+++ SI_day_3/Mission/Mission.Api/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Mission.Services.IServices;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly ILoginService _loginService;
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(string EmailAddress, string Password)
+        {
+            var user = _loginService.login(EmailAddress, Password);
+            if (user == null) {
+                return NotFound("please check your email and password");
+            }
+            return Ok("login successfully");
+        }
+    }
+}
